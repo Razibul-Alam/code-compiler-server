067d6e6951d671dc937a51058fd830edc237e0e4
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2022-04-24 23:00:54 +0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const Axios = require("axios");
 const app = express();
-const PORT = 8000;
+require("dotenv").config();
+const dotenv=require('dotenv')
+const PORT =process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
